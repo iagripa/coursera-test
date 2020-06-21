d594705c105cfe33f19eba02aade40f7039c6400
iagripa <ivanna.agripa@gmail.com>
2020-06-21 01:14:25 -0300

    module4-solution

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -1,3 +1,4 @@
+(function (window) {
 // NOTE! The steps in this file are basically identical to the ones you
 // performed in the SpeakHello.js file.
 
@@ -8,7 +9,11 @@
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-// var byeSpeaker =
+var byeSpeaker = {}; //otro
+//var byeSpeaker = new Object(); //ivi
+//byeSpeaker.speak = function() { //ivi
+//	console.log(baySpeaker); //ivi
+//}
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Good Bye";
@@ -16,10 +21,16 @@ var speakWord = "Good Bye";
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
-  console.log(speakWord + " " + name);
+byeSpeaker.speak = function (name) {
+	console.log(speakWorld + " " + name);
 }
+
+//function speak(name) {
+//console.log(speakWorld + "" + baySpeaker.names); //ivi
+//}
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
-// xxxx.xxxx = byeSpeaker;
+window.byeSpeaker = byeSpeaker;
+
+})(window);
--- module4-solution/script.js
+++ module4-solution/script.js
@@ -33,16 +33,26 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function () {
+(function (window) { //ivi ok
+//var helloSpeaker = {}; //ivi
+  //helloSpeaker.names = function() { //ivi
+    //console.log(speakWorld + "" + helloSpeaker.names); //ivi
+  //}
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  //window.helloSpeaker = helloSpeaker; //ivi
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]; //ok
 
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
+//for (var i in names) { //otro
 for (var i = 0; i < names.length; i++) {
+  //console.log("Hello " + names[i]); //ivi
+  //console.log("Good Bye " + names[i]); //ivi
+//}
 
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
@@ -52,6 +62,9 @@ for (var i = 0; i < names.length; i++) {
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
   var firstLetter = names[i].charAt(0).toLowerCase();
+    //console.log(firstLetter.toLowerCase("J"));
+    //console.log(firstLetter.toUpperCase("j"));
+  //}
 
   // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
@@ -59,10 +72,9 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
+    window.byeSpeaker.speak(names[i]); //otro
   } else {
-    helloSpeaker.speak(names[i]);
+    window.helloSpeaker.speak(names[i]); //otro
   }
 }
-
-})();
+})(window); //ivi ok
