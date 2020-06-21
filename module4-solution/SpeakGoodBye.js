dad67bc2e4f7296211f1e7eae137dab727f4a9a4
iagripa <ivanna.agripa@gmail.com>
2020-06-21 01:35:21 -0300

    module4-solution

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -22,7 +22,7 @@ var speakWord = "Good Bye";
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 byeSpeaker.speak = function (name) {
-	console.log(speakWorld + " " + name);
+	console.log(speakWord + " " + name);
 }
 
 //function speak(name) {
