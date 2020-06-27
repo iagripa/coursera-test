1243c804fe52899661784d8ee3af23de56c4bda8
iagripa <ivanna.agripa@gmail.com>
2020-06-27 16:45:21 -0300

    module5-solution

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -83,9 +83,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  function (buildAndShowHomeHTML){ //IVI
+    document.querySelector("#main-content") //IVI
+    .innerHTML = buildAndShowHomeHTML; //IVI
+  }, // ***** <---- TODO: STEP 1: Substitute [...] ****** IVI
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
-});
+}); //IVI
 // *** finish **
 
 
@@ -101,7 +104,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
+      var chosenCategoryShortName = chooseRandomCategory(categories); //IVI 
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -115,13 +118,16 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      // var homeHtmlToInsertIntoMainPage = ....
+      var homeHtmlToInsertIntoMainPage = "snippets/home-snippet.html"; //IVI
 
 
-      // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
+      // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      // ....
+      var insertHtml = function (selector, html) { //IVI
+        var targetElem = document.querySelector(selector); //IVI
+        targetElem.innerHTML = html; //IVI
+      }; //IVI
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
